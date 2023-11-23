f90dd07bf7558af2cf2a280dbf70bc64863642be
Cristian Lungu <lungucristian95@gmail.com>
2023-11-23 09:08:52 +0200

    init

---
+++ init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("67aba385ce7ca220573299687ce330036c5b9cb53775713667f39314a12268d6" default))
+ '(org-agenda-files '("/home/cl/org/life.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
